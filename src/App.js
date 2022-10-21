aaa6cd0f3957c37a37e5031d207f923ba572e3cb
mxh0000 <112454150+mxh0000@users.noreply.github.com>
2022-10-21 12:57:30 -0500

    mmm

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './Components/dashboard';
 import Preferences from './Components/preferences';
 import Login from './Components/Login/login';
-
+import AboutUs from './Components/AboutUs';
 //dashbord and preferences are place holders for the stuff after the log in page
 
 function App() {
 
   return (
     <div className="Wrapper">
-      <Login></Login>
+      <AboutUs> </AboutUs>
     </div>
   );
 }
---
+++ src/Components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutUs.css';
+
+export default function AboutUs() {
+   
+    return (
+   <div className = "AboutUs">
+        <h1> About Us </h1>
+        <p> We are a grocbdgbtrgrup of students practicing ReactJS to prepare for the upcoming UTD Hackathon</p>
+    </div>
+    )
+}
