2cd3f016a85f55e457c658c38d50af3cd6c9ad69
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2022-10-21 13:06:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Dashboard from './Components/dashboard';
 import Preferences from './Components/preferences';
 import Login from './Components/Login/login';
 import AboutUs from './Components/AboutUs';
+import Homepage from './Components/Homepage/homepage';
+import Image from './Components/Image/image';
+import Navbar from './Components/Navbar/navbar';
+import Loginform from './Components/Login/login';
 //dashbord and preferences are place holders for the stuff after the log in page
 
 function App() {
@@ -13,6 +17,15 @@ function App() {
   return (
     <div className="Wrapper">
       <AboutUs> </AboutUs>
+      <BrowserRouter>
+      <Navbar></Navbar>
+      <Homepage></Homepage>
+      <Image></Image>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard/>}> </Route>
+        <Route path='/preferences' element={<Preferences/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
