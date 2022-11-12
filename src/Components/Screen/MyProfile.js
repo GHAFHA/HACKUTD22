ebadf95633f587fa927af785bd8290490dd80d4e
AllanAbraham23 <allanmatt09@gmail.com>
2022-11-12 17:46:09 -0600

    Update MyProfile.js

--- src/Components/Screen/MyProfile.js
+++ src/Components/Screen/MyProfile.js
@@ -5,7 +5,7 @@ export default function MyProfile() {
    
     return (
    <div className = "AboutUs">
-        <h1> About Us </h1>
+        <h1> My Profile </h1>
         <p> We are a group of students practicing ReactJS to prepare for the upcoming UTD Hackathon</p>
     </div>
     )
