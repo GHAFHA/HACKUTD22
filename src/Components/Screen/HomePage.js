719f526d1df4bccd16ac1e9dcd7c08a6b7df8005
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2022-11-12 17:30:06 -0600

    installed npm react-bootstrap

--- src/Components/Screen/HomePage.js
+++ src/Components/Screen/HomePage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './AboutUs.css';
+import "./HomePage.css";
 
 export default function HomePage() {
    
     return (
-   <div className = "AboutUs">
-        <h1> About Us </h1>
-        <p> We are a group of students practicing ReactJS to prepare for the upcoming UTD Hackathon</p>
+   <div id="homepage-container">
+        
     </div>
     )
 }
