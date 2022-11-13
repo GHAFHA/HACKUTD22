b001c4dd4c770f4514d444a279567011e18b1a87
AllanAbraham23 <allanmatt09@gmail.com>
2022-11-12 21:32:11 -0600

    Chart implemented

--- src/Components/Screen/MyProfile.js
+++ src/Components/Screen/MyProfile.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './AboutUs.css';
-
+import { Container } from 'react-bootstrap';
+import './MyProfile.css';
+import PieRechartComponent from './TestChart';
 export default function MyProfile() {
    
     return (
    <div className = "AboutUs">
+<<<<<<< Updated upstream
         <h1> My Profile </h1>
         <p> We are a group of students practicing ReactJS to prepare for the upcoming UTD Hackathon</p>
+=======
+        <h1> My Profile</h1>
+        
+        <div id="chartDiv"> <PieRechartComponent></PieRechartComponent> </div>
+        
+        
+        
+>>>>>>> Stashed changes
     </div>
+    
     )
 }
