ca9abdf63d2c98d0f60da7e3b6222144dbbd9301
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2022-11-13 10:54:51 -0600

    finished implementation of homepage

--- src/Components/RechartFiles/portfoliochart.js
+++ src/Components/RechartFiles/portfoliochart.js
@@ -3,45 +3,39 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 
 const data = [
   {
-    name: 'Page A',
+    name: 'Time',
     uv: 4000,
-    pv: 2400,
+    cash: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
     uv: 3000,
-    pv: 1398,
+    cash: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
     uv: 2000,
-    pv: 9800,
+    cash: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
     uv: 2780,
-    pv: 3908,
+    cash: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
     uv: 1890,
-    pv: 4800,
+    cash: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
     uv: 2390,
-    pv: 3800,
+    cash: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
     uv: 3490,
-    pv: 4300,
+    cash: 7000,
     amt: 2100,
   },
 ];
@@ -68,8 +62,7 @@ export default class Example extends PureComponent {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="cash" stroke="#8884d8" activeDot={{ r: 8 }} />
         </LineChart>
       </ResponsiveContainer>
     );
--- src/Components/Screen/HomePage.js
+++ src/Components/Screen/HomePage.js
@@ -22,29 +22,29 @@ export default function HomePage() {
                             div
                         </div>
                         <input id="filter-input" placeholder="Enter a filter"></input>
-                        <Button variant="primary">Filter</Button>
+                        <Button id="filter-button">Filter</Button>
                     </table>
                     <table id="trending-container">
                      <h1>Trending</h1>
-                        <div className='text-container'>
+                        <div className='text-container-pos'>
                             <h1>Ford</h1>
                             <h2>General Sentiment: Positive</h2>
+                            <h3 className='value-container'>$$</h3>
                         </div>
                         <div className='text-container-neg'>
                             <h1>3M</h1>
                             <h2>General Sentiment: Negative</h2>
+                            <h3 className='value-container'>$$</h3>
                         </div>
-                        <div className='text-container'>
+                        <div className='text-container-pos'>
                             <h1>Abbott Labs</h1>
                             <h2>General Sentiment: Positive</h2>
+                            <h3 className='value-container'>$$</h3>
                         </div>
                         <div className='text-container-neg'>
                             <h1>Accenture</h1>
                             <h2>General Sentiment: Negative</h2>
-                        </div>
-                        <div className='text-container'>
-                            <h1>AIG</h1>
-                            <h2>General Sentiment: Positive</h2>
+                            <h3 className='value-container'>$$</h3>
                         </div>
                     </table>
                 </Row>
